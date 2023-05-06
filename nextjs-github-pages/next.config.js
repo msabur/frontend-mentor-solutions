622e62c1ba60234a7ceff56aaf64396a0929f030
msabur <58122827+msabur@users.noreply.github.com>
2023-05-06 01:10:48 -0400

    add nextjs thing

---
+++ nextjs-github-pages/next.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: 'export',
+  basePath: '/frontend-mentor-solutions/nextjs-github-pages',
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = nextConfig
